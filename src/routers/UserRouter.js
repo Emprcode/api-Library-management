545214d878ecbf5bb12b32f1e74123a26492b34a
Narayan Aryal <aryal.coding@gmail.com>
2023-01-14 09:56:32 +1100

    some changes made

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { hashPassword } from "../helpers/bcryptHelper.js";
+import { comparePassword, hashPassword } from "../helpers/bcryptHelper.js";
 import { createUser, getUserByEmail } from "../models/users/UserModel.js";
 
 const router = express.Router();
 
+// post for registration
 router.post("/", async (req, res, next) => {
   const { email } = req.body;
   try {
@@ -37,23 +38,37 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.get("/", (req, res, next) => {
-  res.json({
-    status: "success",
-    message: "Connected",
-  });
-});
-router.put("/", (req, res, next) => {
-  res.json({
-    status: "success",
-    message: "updated",
-  });
-});
-router.delete("/", (req, res, next) => {
-  res.json({
-    status: "success",
-    message: "deleted",
-  });
+// post for login
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    const user = await getUserByEmail(email);
+    if (user?._id) {
+      const isPassMatch = comparePassword(req.body.password, user.password);
+      if (isPassMatch) {
+        user.password = undefined;
+        return res.json({
+          status: "success",
+          message: "Login Successful",
+          user,
+        });
+      }
+      res.json({
+        status: "error",
+        message: "user not found! Please try again later",
+      });
+    } else {
+      res.json({
+        status: "error",
+        message: "user not found! Please try again later",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 export default router;
